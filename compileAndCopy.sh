19561406c5070b284d353e4c25ea4f23d2100091
clegoff <c.legoffpro@gmail.com>
2019-06-25 12:02:10 +0200

    ERR3

--- compileAndCopy.sh
+++ compileAndCopy.sh
@@ -1,4 +1,4 @@
 make clean; make;
 sudo mount /dev/sda /media/nucleo;
-sudo cp .build/course_instabot.bin /media/nucleo/;sync;
+sudo cp .build/course_instabot401_2.bin /media/nucleo/;sync;
 sudo umount /media/nucleo
