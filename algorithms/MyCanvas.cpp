c4a27e2194f584361bfd804a8b4e5c8b9d19c180
Miguel <m.dacostarego@gmail.com>
2019-06-26 14:11:09 +0200

    ERR5

--- algorithms/MyCanvas.cpp
+++ algorithms/MyCanvas.cpp
@@ -7,50 +7,30 @@ int pasDeJeuCourrant;
 
 void MyCanvas::activate() {
     pasDeJeuCourrant=0;
-    //wait(1);
-    //actionMoteur->initHeadingDeg();
     return;
 }
 
 void MyCanvas::step() {
 
     if (pasDeJeuCourrant>MAXPASDEJEU) {return;}
+
     pasDeJeuCourrant++;
 
-
-    //SOME RANDOM STUFF
-    /*if (pasDeJeuCourrant>=121){
-        if (readCounterGauche()>=10000 && readCounterDroite() >=10000) {
-            moveBack();
-            if (readCounterGauche()>=15000 && readCounterDroite()>=15000){
-                resetCounterDroite();
-                resetCounterGauche();
-            }
-        } else {
-            move();
-        }
-        return;
-    }
-    if (pasDeJeuCourrant>=120){
-        resetCounterGauche();
-        resetCounterDroite();
-    }*/
-
-    if (pasDeJeuCourrant>=200){
+    if (pasDeJeuCourrant>=180){
         move();
         return;
     }
 
-    if (pasDeJeuCourrant>=160){
+    if (pasDeJeuCourrant>=150){
         stepTurn(LEFT);
         return;
     }
 
-    if (pasDeJeuCourrant>=130){
+    if (pasDeJeuCourrant>=120){
         return;
     }
 
-    if (pasDeJeuCourrant>=90){
+    if (pasDeJeuCourrant>=80){
         move();
         return;
     }
